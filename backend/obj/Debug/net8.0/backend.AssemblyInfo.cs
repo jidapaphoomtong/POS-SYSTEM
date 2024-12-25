56e0165b360a3109365e2673a3bbec95b791f9d7
Jidapa Phoomtong <64010912501@msu.ac.th>
2024-12-25 17:34:55 +0700

    file change

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d33e03ed7b470dd549e34037cdbd1dd9cf5bf83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb673b631421c3307557315ec2fc7b279d5a3a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/src/components/branch.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const branch = () => {
+    const [viewMode, setViewMode] = useState("grid");
+    const [branch, setBranch] = useState([]);
+    const [selectBranch, setSelectBranch] = useState(null);
+    
+    return (
+        <div className='container'>
+        
+        </div>
+    )
+}
+
+export default branch
