227f577e0daeb4ec6ba4046d2dc067c60738f8ca
Jidapa Phoomtong <144982326+jidapaphoomtong@users.noreply.github.com>
2025-01-12 22:55:05 +0700

    update product

--- backend/Services/ProductService/IProductService.cs
+++ backend/Services/ProductService/IProductService.cs
@@ -15,7 +15,11 @@ namespace backend.Services.ProductService
         Task<string> GetNextId(string sequenceName);
         Task<ServiceResponse<string>> AddProduct(string branchId, Products product);
         Task<ServiceResponse<List<object>>> GetProducts(string branchId);
+        Task<ServiceResponse<Products>> GetProductById(string branchId, string productId);
+        Task<ServiceResponse<string>> SellProduct(string branchId, string productId, int quantity);
         Task<ServiceResponse<string>> UpdateProduct(string branchId, string productId, Products updatedProduct);
         Task<ServiceResponse<string>> DeleteProduct(string branchId, string productId);
+        Task<ServiceResponse<string>> AddStock(string branchId, string productId, int quantity);
+        Task<ServiceResponse<string>> ReduceStock(string branchId, string productId, int quantity);
     }
 }
