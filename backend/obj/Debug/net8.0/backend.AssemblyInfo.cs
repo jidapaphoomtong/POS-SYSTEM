b8d62e8784c7a7e749cea37e2f8866e9358b1631
Jidapa Phoomtong <64010912501@msu.ac.th>
2025-01-15 11:29:32 +0700

    update file

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e60eb89bf37dcec8c8b73e9adace59541b388dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c476fe5e62b5a32d2becea4bb5fcf5a95ce9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
